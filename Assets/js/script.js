09838c67b55aefb3751fc80adec7796ec14c99f8
Alkida Vaci <alkida.vaci@gmail.com>
2022-10-29 14:59:51 -0400

    Get text from local storege and show in page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,3 +18,21 @@ $(".saveBtn").on("click", function () {
     // 'this' is for the button where the event happen
     toDo($(this));
 });
+
+//Each time get the toDoText from local storage and put the value in textarea 
+$("#7 .description").val(localStorage.getItem("7"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("0"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
+$("#21 .description").val(localStorage.getItem("21"));
+$("#22 .description").val(localStorage.getItem("22"));
